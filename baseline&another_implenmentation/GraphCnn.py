226e37f9fac1c768ad6107fb0c297a7fbccc2f9e
zhang yang <43399212+zhangyang2019@users.noreply.github.com>
2019-05-05 15:02:08 +0800

    Update GraphCnn.py

--- baseline&another_implenmentation/GraphCnn.py
+++ baseline&another_implenmentation/GraphCnn.py
@@ -81,7 +81,7 @@ class GCNs(nn.Module):
             #pre_layer.append(nn.BatchNorm1d(pre_dim))
             pre_input_dim = pre_dim
         pre_layer.append(nn.Linear(pre_input_dim, class_dim))
-        pre_layer.append(nn.ReLU())
+        #pre_layer.append(nn.ReLU())           # the last layer before softmax, don't need activate.
         pred_model = nn.Sequential(*pre_layer)
         return pred_model
 
